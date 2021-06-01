ee99290476e4de49239b16c7c308b627299d2765
katsuya_U <katsuya_u@icloud.com>
2021-06-01 13:14:04 +0900

    fix: 全体のcssを調整

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import "normalize.css"
+import "../src/assets/index.css"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "normalize.css"
 import "./assets/index.css"
-import App from "./App"
+import { App } from "./App"
 
 ReactDOM.render(
   <React.StrictMode>
